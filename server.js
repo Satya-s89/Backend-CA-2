04cf22cf0feb137181348d58bc5e550bcc048039
Satya-s89 <kaki.harisatya.s89@kalvium.community>
2025-04-07 15:04:03 +0530

    ca-2

--- server.js
+++ server.js
@@ -5,22 +5,25 @@ const app = express();
 app.use(express.json());
 
 const userData = [
-    {userName: "satya","age": 18, email:"Satya123@gmail.com"},
-    {userName: "akhil","age": 17, email:"akhil45@gmail.com"},
-    {userName: "mourya","age": 19, email:"mourya789@gmail.com"},
-    {userName: "madhu","age": 20, email:"madhu7890@gmail.com"}
+    {id:1,userName: "satya","age": 18, email:"Satya123@gmail.com"},
+    {id:2,userName: "akhil","age": 17, email:"akhil45@gmail.com"},
+    {id:3,userName: "mourya","age": 19, email:"mourya789@gmail.com"},
+    {id:4,userName: "madhu","age": 20, email:"madhu7890@gmail.com"}
 ];
 
-app.get("/user",(req,res) => {
-    const user = req.params;
-    if(!user){
+app.get("/user/:id",(req,res) => {
+    const id = req.params;
+    if(!id){
         return res.status(404).send({msg:"User parameter cannot be empty"});
     }
-    try {
-        return res.status(200).send({msg:"User found" , user:{userData}});
-    }catch {
-        return res.status(500).send({msg:"User not found"});
-    }
+    userData.map((ele) => {
+        if(ele == id){
+            return res.status(200).send({msg:"User found" , user:{userData}});
+        }
+        else{
+            return res.status(500).send({msg:"User not found"});
+        }
+    });
 })
 
 
